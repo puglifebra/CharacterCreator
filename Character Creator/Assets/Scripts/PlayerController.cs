d7a34b23dc31ff523792824dc02f389e803aee82
puglifebra <ghobart2024@chadwickschool.org>
2021-12-07 10:35:28 -0800

    feat: Add weapon shop

--- Character Creator/Assets/Scripts/PlayerController.cs
+++ Character Creator/Assets/Scripts/PlayerController.cs
@@ -6,10 +6,15 @@ public class PlayerController : MonoBehaviour
 {
 
     public SpriteRenderer ArmorRenderer;
+    public SpriteRenderer WeaponRenderer;
     // Start is called before the first frame update
     public void setArmor(Sprite newSprite)
     {
         ArmorRenderer.sprite = newSprite;
+    }
+    public void setWeapon(Sprite newSprite)
+    {
+        WeaponRenderer.sprite = newSprite;
     }
     void Start()
     {
