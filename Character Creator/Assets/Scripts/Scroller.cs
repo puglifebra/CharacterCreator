9107bd2be1d3bb30dfca9ccfa17e49ec03f4f0b7
puglifebra <ghobart2024@chadwickschool.org>
2021-12-13 10:38:03 -0800

    Work

--- Character Creator/Assets/Scripts/Scroller.cs
+++ Character Creator/Assets/Scripts/Scroller.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Scroller : MonoBehaviour
 {
-    [SerializeField] private RawImage _img;
+    // [SerializeField] private Image _img;
     [SerializeField] private float _x, _y;
+    public float resetX;
 
     // Update is called once per frame
     void Update()
     {
-        _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x,_y) * Time.deltaTime,_img.uvRect.size);
+        // _img.uvRect = new Rect(_img.uvRect.position + new Vector2(_x,_y) * Time.deltaTime,_img.uvRect.size);
+        float newX = this.transform.position.x + _x * Time.deltaTime;
+        float newY = this.transform.position.y + _y * Time.deltaTime;
+        this.transform.position = new Vector2(newX, newY);
+        Debug.Log($"X: {this.transform.position.x}");
+        if (this.transform.localPosition.x < -580) 
+        {
+
+        this.transform.localPosition = new Vector2(resetX, 0);
+  
+        }
     }
 }
